f7abf38b283b9cd9f806f34f4a8344b12c6f88d9
mgarcia1g <gg.mgarcia@outlook.com>
2023-05-15 20:46:14 -0400

    fixed background. ios does not take fixed in css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import BusinessLogoComponent from './components/cards-component/businessLogoComponent/businessLogoComponent';
-
 function App() {
 
   return (
     <div className="App">
+      <div className="content"> 
       <BusinessLogoComponent />
+
+      </div>
     </div>
   );
 }
